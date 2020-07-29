98af577686be3205c2f6091bfe46a07da31e923c
FunMan22 <43394783+FunMan22@users.noreply.github.com>
2020-07-28 22:46:22 -0400

    Clean Up 2

--- nodeMain.js
+++ nodeMain.js
@@ -6,6 +6,7 @@ var url = require('url');
 var fs = require('fs');
 var path = require('path');
 var dotenv = require('dotenv').config({ path: 'C:\\Users\\lucas\\Documents\\YTChatConfig.env' }/*Probably not needed, more secure?*/);
+var port = 8080;
 
 http.createServer(function(req, res) {
     if (req.url === '/') {
@@ -27,41 +28,6 @@ http.createServer(function(req, res) {
             return;
         });
     }
-}).listen(8080);
+}).listen(port);
 
-
-/*var http = require('http');
-var fs = require('fs');
-var url = require('url');
-
-
-http.createServer(function (req, res) {  
-    var path = url.parse(req.url).pathname;
-    path = path.normalize;
-    if ((path = '/') || (path = '/index.html')) {
-        fs.readFile('index.html', function(err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-          });
-    }
-    else {
-        res.writeHead(404);
-        res.write('<h1>We are sorry. The page you are looking for does not exist.</h1>');
-        res.end();
-    }
-}).listen(8080);*/
-
-/*var static = require('node-static');
-var http = require('http');
-
-var file = new(static.Server)();
-
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(8080);*/
-
-console.log(process.env.API_KEY);
-var API_KEY = process.env.API_KEY;
-
-console.log("End");
+console.log("Server now Running on Port " + port);
